a3e57d4b8bbcd7bb94f2d1f08393e7fbd6006fc7
willker <wz.willker@gmail.com>
2026-06-04 21:41:39 +0800

    nabu: Add fake MiuiSecurityCenter

--- device.mk
+++ device.mk
@@ -480,5 +480,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+PRODUCT_PACKAGES += \
+    MiuiSecurityCenter
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/nabu/nabu-vendor.mk)
